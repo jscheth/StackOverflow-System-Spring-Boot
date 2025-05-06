c9fde6f4f84e4bc92ef760984316dead1e62ebe8
jscheth <sovichea.chet@gmail.com>
2025-05-06 12:35:09 -0700

    Git init

---
+++ src/main/java/com/cybercitycode/stackoverflowsystem/StackOverflowSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cybercitycode.stackoverflowsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackOverflowSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StackOverflowSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cybercitycode/stackoverflowsystem/StackOverflowSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybercitycode.stackoverflowsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackOverflowSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
